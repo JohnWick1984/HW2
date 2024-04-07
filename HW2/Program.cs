6519854d64cea7c78a9a68579266dc0d4abcecf2
Евгений Шумаков <daredevil1984@list.ru>
2024-04-07 09:50:46 +0500

    Добавьте файлы проекта.

---
+++ HW2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Запускаем блокнот
+            Process.Start("notepad.exe");
+            Console.WriteLine("Блокнот запущен.");
+
+            // Запускаем Paint
+            Process.Start("mspaint.exe");
+            Console.WriteLine("Paint запущен.");
+
+            // Запускаем калькулятор
+            Process.Start("calc.exe");
+            Console.WriteLine("Калькулятор запущен.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Произошла ошибка при запуске приложений: " + e.Message);
+        }
+    }
+}
